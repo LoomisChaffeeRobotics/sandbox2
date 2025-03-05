7ba5feace33b64ad67743e7d8e4957423b93fc04
violin2024 <musichu20171819@gmail.com>
2025-03-05 09:38:16 -0500

    Comments + drive encoder localizer code added
    template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.constants;
 
+import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.util.CustomFilteredPIDFCoefficients;
@@ -8,7 +9,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class FConstants {
     static {
-        FollowerConstants.localizers = Localizers.THREE_WHEEL;
+
+        //tune this after LConstants
+
+        //before tuning each PID, go to follower tab + enable/disable use___ to make sure you oNLY USE ONE PID AT A TIME
+
+        FollowerConstants.localizers = Localizers.DRIVE_ENCODERS; //or 3 wheel imu
 
         FollowerConstants.leftFrontMotorName = "leftFront";
         FollowerConstants.leftRearMotorName = "leftRear";
@@ -20,29 +26,34 @@ public class FConstants {
         FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
         FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.mass = 13; //in kg
 
-        FollowerConstants.xMovement = 57.8741;
-        FollowerConstants.yMovement = 52.295;
+        FollowerConstants.xMovement = 57.8741; //run ForwardVelocityTuner + put velocity output here
+        FollowerConstants.yMovement = 52.295; //run StrafeVelocityTuner + repeat
 
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+        FollowerConstants.forwardZeroPowerAcceleration = -41.278; //run ForwardZeroPowerAccelerationTuner + get output
+        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;//run LateralZeroPowerAccelerationTuner + same
 
+        //use StraightBackandForth opmode, push robot to test correction
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
-        FollowerConstants.useSecondaryTranslationalPID = false;
+        FollowerConstants.useSecondaryTranslationalPID = false; //if true set all 3
         FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
 
+        //use StraightBackandForth opmode, Repeat the tuning process with varying rotation angles and directions to ensure consistent performance.
         FollowerConstants.headingPIDFCoefficients.setCoefficients(2,0,0.1,0);
         FollowerConstants.useSecondaryHeadingPID = false;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
+        //use StraightBackandForth opmode, READ THE FREAKING WEBSITE A
         FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
         FollowerConstants.useSecondaryDrivePID = false;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
         FollowerConstants.zeroPowerAccelerationMultiplier = 4;
-        FollowerConstants.centripetalScaling = 0.0005;
+        FollowerConstants.centripetalScaling = 0.0005; //use CurvedBackandForth opmode
+        //if it goes on the inside increase it, if it goes on outside decrease it
 
+        //not changed for tuning
         FollowerConstants.pathEndTimeoutConstraint = 500;
         FollowerConstants.pathEndTValueConstraint = 0.995;
         FollowerConstants.pathEndVelocityConstraint = 0.1;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -2,21 +2,52 @@ package org.firstinspires.ftc.teamcode.pedroPathing.constants;
 
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        DriveEncoderConstants.forwardTicksToInches = 1; //push robot + use multiplier (2nd number) here
+        DriveEncoderConstants.strafeTicksToInches = 1; //push robot + use multiplier (2nd number) here
+        DriveEncoderConstants.turnTicksToInches = 1; //spin robot + use multiplier (2nd number) here
+
+        DriveEncoderConstants.robot_Width = 1; //distance from side eto side wheels
+        DriveEncoderConstants.robot_Length = 1; //distance from forward to back wheels
+
+        //when robot move forward all should go up
+        DriveEncoderConstants.leftFrontEncoderDirection = Encoder.REVERSE; //make sure these are good first
+        DriveEncoderConstants.rightFrontEncoderDirection = Encoder.FORWARD;
+        DriveEncoderConstants.leftRearEncoderDirection = Encoder.REVERSE;
+        DriveEncoderConstants.rightRearEncoderDirection = Encoder.FORWARD;
+
+        //below for 3 wheel IMU
+        ThreeWheelIMUConstants.forwardTicksToInches = .001989436789; //same as drive encoders
+        ThreeWheelIMUConstants.strafeTicksToInches = .001989436789; //same
+        ThreeWheelIMUConstants.turnTicksToInches = .001989436789; //same
+        ThreeWheelIMUConstants.leftY = 1; //position of left wheel that move forward when you forward
+        ThreeWheelIMUConstants.rightY = -1; //position of right wheel that forward when forwqrd
+        ThreeWheelIMUConstants.strafeX = -2.5; //wheel that no forward when forward - negative = behind center of mass
+        ThreeWheelIMUConstants.leftEncoder_HardwareMapName = "leftFront";
+        ThreeWheelIMUConstants.rightEncoder_HardwareMapName = "rightRear";
+        ThreeWheelIMUConstants.strafeEncoder_HardwareMapName = "rightFront";
+        //if x value goes down when moves forward, both LR need to be switched
+        //if x value stays constant only one has to change
+        //if y value goes down when strafe left reverse strafe
+        ThreeWheelIMUConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelIMUConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelIMUConstants.strafeEncoderDirection = Encoder.FORWARD;
+        ThreeWheelIMUConstants.IMU_HardwareMapName = "imu";
+        ThreeWheelIMUConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
+//        If your robot seems to:
+//
+//        Turn to face a different direction when starting a path
+//        Actively turning to a fully incorrect angle (or miss with a large untunable/unfixable error)
+//        Your robot's IMU may have interference caused by ESD (electrostatic discharge).
+//
+//        Consider grounding the robot with a grounding strap and reading this guide from FIRST to understand ESD more.
+//
+//                If after all of this you cannot fix the issue, switch to the non-IMU ThreeWheel localizer, as it will not be as harshly affected by ESD and have more accuracy (compared to an interfered IMU).
+
+
     }
 }
 
